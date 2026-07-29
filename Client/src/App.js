d60ed65fe4db7b039a760e20d6db6b5ffb01061c
Komma Anushka <251626100+AnushkaReddyNITRR@users.noreply.github.com>
2026-07-29 18:00:05 +0530

    Refactor App component to include Sidebar

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,13 @@
 import ObligationTracker from './features/obligation-tracker/ObligationTracker'
+import Sidebar from './features/obligation-tracker/components/Sidebar'
 
 export default function App() {
-  return <ObligationTracker />
+  return (
+    <div className="app-shell">
+      <Sidebar activeKey="obligations" />
+      <div className="main-panel">
+        <ObligationTracker />
+      </div>
+    </div>
+  )
 }
