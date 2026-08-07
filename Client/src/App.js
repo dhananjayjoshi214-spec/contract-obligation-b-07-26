9abdc324622e5e546f1bf62a17c15680f57eead4
Komma Anushka <251626100+AnushkaReddyNITRR@users.noreply.github.com>
2026-08-07 23:44:33 +0530

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,20 @@
 import ObligationTracker from './features/obligation-tracker/ObligationTracker'
 import Sidebar from './features/obligation-tracker/components/Sidebar'
 
+
+
+
 export default function App() {
   return (
+    
+    
     <div className="app-shell">
       <Sidebar activeKey="obligations" />
       <div className="main-panel">
         <ObligationTracker />
       </div>
     </div>
+  
   )
+  
 }
